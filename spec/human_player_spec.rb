67a815d07870cd0f745838e2062b993d8dfc9606
Gary Miller <gary.miller@gmx.com>
2020-05-09 09:45:49 -0400

    Update human_player_spec.rb

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,6 +1,6 @@
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/Tic_Tac_Toe
 
 
 require "human_player"
@@ -44,4 +44,4 @@ end
 
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/Tic_Tac_Toe
