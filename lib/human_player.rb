217174f82c98346ce0f24646c88e9090e0e53a6d
Gary Miller <gary.miller@gmx.com>
2020-05-09 09:44:51 -0400

    Update human_player.rb

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,6 +1,6 @@
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/Tic_Tac_Toe
 
 
 class HumanPlayer
@@ -53,4 +53,4 @@ end
 
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/Tic_Tac_Toe
